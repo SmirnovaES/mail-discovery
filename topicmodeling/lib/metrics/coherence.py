aaf9b7c2aa6e2b98b7f43581d2c2297353b3c3eb
geras-artem <geras-artem@yandex.ru>
2018-03-24 13:31:01 +0300

    inicializing commit

---
+++ topicmodeling/lib/metrics/coherence.py
@@ -0,0 +1,12 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+class Coherence:
+    def __init__(self, corpus, words, topics):
+        self.corpus = corpus
+        self.words = words
+        self.topics = topics
+
+    def calulate(self):
+    pass
