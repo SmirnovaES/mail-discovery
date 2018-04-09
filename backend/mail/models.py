2ba8be8f29e0837364e223804f907523cac78e55
Elena Smirnova <elena_smirnova98@mail.ru>
2018-04-09 07:10:57 +0300

    postgres added?

--- backend/mail/models.py
+++ backend/mail/models.py
@@ -1,22 +1,48 @@
-from django.db import models
-from datetime import datetime
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
 
 class mails(models.Model):
-  Date = models.DateTimeField(default=datetime.now, blank=True)
-  NameFrom = models.CharField(max_length=100, blank=True)
-  AddressFrom = models.CharField(max_length=100, blank=True)
-  NameTo = models.CharField(max_length=100, blank=True)
-  AddressTo = models.CharField(max_length=100, blank=True)
-  Subject = models.CharField(max_length=100, blank=True)
-  Message = models.TextField(blank=True)
+    id = models.IntegerField(primary_key=True)
+    date = models.DateTimeField(blank=True, null=True)
+    namefrom = models.TextField(blank=True, null=True)
+    addressfrom = models.TextField(blank=True, null=True)
+    nameto = models.TextField(blank=True, null=True)
+    addressto = models.TextField(blank=True, null=True)
+    subject = models.TextField(blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'mails'
+
 
 class users(models.Model):
-  Name = models.CharField(max_length=100, blank=True)
-  Address = models.CharField(max_length=100, blank=True)
-  DateBegin = models.DateTimeField(default=datetime.now, blank=True)
-  DateEnd = models.DateTimeField(default=datetime.now, blank=True)
-  Department = models.CharField(max_length=100, blank=True)
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    department = models.TextField(blank=True, null=True)
+    datebegin = models.DateTimeField(blank=True, null=True)
+    dateend = models.DateTimeField(blank=True, null=True)
 
-
+    class Meta:
+        managed = False
+        db_table = 'users'
