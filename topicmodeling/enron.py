fc2ae7c6659d8c270d1f843ec4d8c6e6c7093aae
littleboyjohnny <lexkoz.yan@gmail.com>
2018-04-21 16:19:20 +0000

    added ldamulticore

--- topicmodeling/enron.py
+++ topicmodeling/enron.py
@@ -1,25 +1,37 @@
+import nltk
+import matplotlib
+matplotlib.use('Agg')
+import sys
+import time
+sys.path.append('/home/lexkoz_yan/MailDiscoveryML')
+#nltk.download()
+
 from topicmodeling.lib.visualization.visualizer import Visualizer
 from topicmodeling.lib.baseline.lsa import LsaModel
 from topicmodeling.lib.baseline.lda import LdaModel
+from topicmodeling.lib.baseline.ldamulticore import LdaMulticoreModel
 
-
-path = '/home/geras-artem/Desktop/'
-corpus_path = '/home/geras-artem/Desktop/Study/6sem/InPrak/MailDiscoveryML/data/maildir/'
+path = '/home/lexkoz_yan/results/ldamodels/test/'
+corpus_path = '/home/lexkoz_yan/maildiscovery/maildir/'
+modelpath = '/home/lexkoz_yan/models/ldamodels/test/ldamodel'
 
 
 def main():
-    model = LdaModel()
-    model.load(path='/home/geras-artem/Desktop/ldamodel')
+    model = LdaMulticoreModel()
+    #model.load(path=modelpath)
     model.extract_corpus(corpus_path)
     model.data_processing()
-    #model.build(num_topics=10, passes=20, path='/home/geras-artem/Desktop/ldamodel')
+    start = time.time()
+    model.build(num_topics=10, passes=1, path=modelpath, workers=7)
+    end = time.time()
+    print(str(end - start))
 
-    docs_words = model.get_docs()
-    topics_words = model.get_topics()
+    #docs_words = model.get_docs()
+    #topics_words = model.get_topics()
 
-    topics_top_terms = model.show_topics(num_topics=10, formatted=False)
+    #topics_top_terms = model.show_topics(num_topics=10, formatted=False)
 
-    Visualizer(docs_words, topics_words, topics_top_terms).run(path, metrics='tfIdf', proc_num=1)
+    #Visualizer(docs_words, topics_words, topics_top_terms).run(path, metrics='tfIdf', proc_num=7)
 
 if __name__ == '__main__':
     main()
