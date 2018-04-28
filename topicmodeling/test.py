442f60506fac9c51d4763a3339fdd43293ac8b3f
geras-artem <geras-artem@yandex.ru>
2018-04-28 20:49:21 +0300

    global restyle patch

---
+++ topicmodeling/test.py
@@ -0,0 +1,9 @@
+from topicmodeling.lib.baseline.lda import LdaModel
+from topicmodeling.output.gettopics import getTopics
+
+
+corpus_path = '/home/geras-artem/Desktop/Study/6sem/InPrak/MailDiscoveryML/part'
+part_path = '/home/geras-artem/Desktop/Study/6sem/InPrak/MailDiscoveryML/data/maildir/arnold-j/sent'
+
+if __name__ == '__main__':
+    print(getTopics(source=part_path))
