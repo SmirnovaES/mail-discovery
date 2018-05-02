6730894bd291bd06d71b8558fee678473980a7e2
EgorovMike219 <mike219@mail.ru>
2018-05-02 12:49:03 +0300

    заменил datetime на char

--- backend/mail/forms.py
+++ backend/mail/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class SearchReqForm(forms.Form):
-  datefrom = forms.DateTimeField()
-  dateto = forms.DateTimeField()
+  datefrom = forms.CharField()
+  dateto = forms.CharField()
   users = forms.CharField() #need splitting
   topics = forms.CharField() #need splitting
   search = forms.CharField()
