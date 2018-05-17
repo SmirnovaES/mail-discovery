fa00f1a56eaadd571405081670b7ba268b224d7a
David Narsia <narsiya.david@yandex.ru>
2018-05-17 03:02:15 +0300

    Small fixes

--- parsing/enron_parser/enron_parser.py
+++ parsing/enron_parser/enron_parser.py
@@ -10,7 +10,7 @@ class Parser:
 
     def create_database(self):
         mails_table.create_mails_table(self.file_name, self.limit)
-        #users_table.create_users_table(self.file_name, self.limit)
+        users_table.create_users_table(self.file_name, self.limit)
 
 parser = Parser('/home/david/почта/enron/emails.csv')
 parser.create_database()
